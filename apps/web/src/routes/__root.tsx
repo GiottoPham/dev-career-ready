2be5192543655744559674aa698b91872d184ff6
Giotto Pham <giotto2015.py@gmail.com>
2026-06-17 17:50:09 +0700

    setup shadcn tanstack router

---
+++ apps/web/src/routes/__root.tsx
@@ -0,0 +1,15 @@
+import { Link, createRootRoute } from "@tanstack/react-router"
+
+import { RootComponent } from "./-Root"
+
+export const Route = createRootRoute({
+  component: RootComponent,
+  notFoundComponent: () => {
+    return (
+      <div>
+        <p>This is the notFoundComponent configured on root route</p>
+        <Link to="/">Start Over</Link>
+      </div>
+    )
+  },
+})
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,27 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import path from "path"
+
+import tailwindcss from "@tailwindcss/vite"
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+      routesDirectory: "./src/routes",
+      generatedRouteTree: "./src/routeTree.gen.ts",
+      routeFileIgnorePrefix: "-",
+      quoteStyle: "single",
+    }),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
